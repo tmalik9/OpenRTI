8f81d4b4a2ed9e754e56408a7b8bdc8560b03f69
Breitsprecher, Konrad <Konrad.Breitsprecher@vector.com>
2020-10-30 12:33:00 +0000

    COE-26618: Disabled OPENRTI_ENABLE_PYTHON_BINDINGS; Corrected include path to DebugNew
    git-svn-id: https://vistrpndsvn1.vi.vector.int/svn/CANoe_Components/OpenRTI/trunk@554565 fc9017fd-cfa4-6c48-8536-0279c867a896

--- python/rti1516e/Module.cpp
+++ python/rti1516e/Module.cpp
@@ -22,7 +22,7 @@
 #include <map>
 #include <set>
 
-#include "DebugNew.h"
+#include "../../src/OpenRTI/DebugNew.h"
 #include "RTI/RTI1516.h"
 #include "RTI/FederateAmbassador.h"
 #include "RTI/time/HLAfloat64Time.h"
