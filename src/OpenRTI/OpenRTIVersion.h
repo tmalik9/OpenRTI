43def19001c3d967a1a19b1d14c5d51ed962b791
Breitsprecher, Konrad <Konrad.Breitsprecher@vector.com>
2021-01-13 11:33:06 +0000

    Updated OpenRTI Version -> 1.6.0.7
    git-svn-id: https://vistrpndsvn1.vi.vector.int/svn/CANoe_Components/OpenRTI/trunk@570993 fc9017fd-cfa4-6c48-8536-0279c867a896

--- src/OpenRTI/OpenRTIVersion.h
+++ src/OpenRTI/OpenRTIVersion.h
@@ -14,8 +14,8 @@
 #define OPENRTI_BUILD_NUMBER_STRING      "0"
 
 // Patchnumber of OPENRTI library
-#define OPENRTI_PATCH_NUMBER              6
-#define OPENRTI_PATCH_NUMBER_STRING      "6"
+#define OPENRTI_PATCH_NUMBER              7
+#define OPENRTI_PATCH_NUMBER_STRING      "7"
 
 #define YEAR_STRING                     "2020"
 
