28e793f8a702d784d15fd17470454d740dca162b
Malik, Thomas <Thomas.Malik@vector.com>
2021-02-23 12:29:46 +0000

    added missing files from last commit
    git-svn-id: https://vistrpndsvn1.vi.vector.int/svn/CANoe_Components/OpenRTI/trunk@578954 fc9017fd-cfa4-6c48-8536-0279c867a896

---
+++ src/Tools/FOMCodeGen/FOMEncodingImplCode.cs
@@ -0,0 +1,17 @@
+﻿namespace FOMCodeGen
+{
+  using System.Linq;
+  using System.Text;
+  using System.Collections.Generic;
+  using System;
+  public partial class FOMEncodingImpl : FOMEncodingImplBase
+  {
+    FOMParser FOM { get; set; }
+    string EncodingHeaderFilename { get; set; }
+    public FOMEncodingImpl(FOMParser fom, string encodingHeaderFilename)
+    {
+      FOM = fom;
+      EncodingHeaderFilename = System.IO.Path.GetFileName(encodingHeaderFilename);
+    }
+  }
+  }
